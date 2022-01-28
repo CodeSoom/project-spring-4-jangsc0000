f42c69151b3f8c26ea013345fef34f22ec477a42
장승철 <jangsc0000@gmail.com>
2022-01-29 06:19:30 +0900

    feat: Add image model

---
+++ src/main/java/com/pio/apiserver/domain/Image.java
@@ -0,0 +1,30 @@
+package com.pio.apiserver.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Builder
+@Getter
+@NoArgsConstructor
+@Entity
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "post_id")
+    private Long postId;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+}

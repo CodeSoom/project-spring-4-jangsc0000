3d6347bc270de59d27156d428a478948ddcb5dbc
장승철 <jangsc0000@gmail.com>
2022-02-02 08:41:36 +0900

    test: detail, delete의 fixture 추가
    - existed post, image 추가

--- src/test/java/com/pio/apiserver/application/ImageServiceTest.java
+++ src/test/java/com/pio/apiserver/application/ImageServiceTest.java
@@ -1,8 +1,15 @@
 package com.pio.apiserver.application;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.pio.apiserver.config.AmazonS3Config;
 import com.pio.apiserver.domain.Image;
+import com.pio.apiserver.domain.Post;
 import com.pio.apiserver.dto.ImageCreationData;
 import com.pio.apiserver.repository.ImageRepository;
+import com.pio.apiserver.repository.PostRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -11,20 +18,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 @DataJpaTest
 @DisplayName("ImageService")
 class ImageServiceTest {
-    ImageService imageService;
-    S3Service s3Service;
+    static final String EXISTED_IMAGE_TYPE = "image/jpeg";
+    static final String EXISTED_IMAGE_URL = "https://test.com/image.jpg";
 
     @Autowired
     ImageRepository imageRepository;
-    
+
+    @Autowired
+    PostRepository postRepository;
+
+    ImageService imageService;
+
+    S3Uploader s3Uploader;
+
     @BeforeEach
     void prepareService() {
-        s3Service = new S3Service();
-        imageService = new ImageService(imageRepository, s3Service);
+        s3Uploader = mock(S3Uploader.class);
+        imageService = new ImageService(imageRepository, s3Uploader);
     }
 
     @Nested
@@ -34,7 +50,8 @@ class ImageServiceTest {
         @DisplayName("이미지 등록 정보가 주어지면")
         class Context_with_image {
             static final String NEW_IMAGE_TYPE = "image/jpeg";
-            static final byte[] NEW_IMAGE_BINS = {0x48, 0x65, 0x6C, 0x6C, 0x6f, 0x20, 0x57, 0x6f, 0x72, 0x6c, 0x64};
+            static final byte[] NEW_IMAGE_BINS = {0x48, 0x65, 0x6C, 0x6C};
+            static final String NEW_IMAGE_URL = "https://test.com/image.jpg";
 
             ImageCreationData imageCreationData;
 
@@ -47,6 +64,8 @@ class ImageServiceTest {
                         .postId(post_id)
                         .image(NEW_IMAGE_BINS)
                         .build();
+
+                given(s3Uploader.upload(NEW_IMAGE_BINS, NEW_IMAGE_TYPE)).willReturn(NEW_IMAGE_URL);
             }
 
             @Test
@@ -63,11 +82,18 @@ class ImageServiceTest {
     @Nested
     @DisplayName("detail 메소드는")
     class Describe_detail {
+
         Image existed_image;
 
         @BeforeEach
         void prepare() {
-            existed_image = Image.builder().build();
+            Post post = postRepository.save(Post.builder().build());
+
+            existed_image = Image.builder()
+                    .postId(post.getId())
+                    .imageUrl(EXISTED_IMAGE_URL)
+                    .contentType(EXISTED_IMAGE_TYPE)
+                    .build();
             imageRepository.save(existed_image);
         }
 
@@ -93,7 +119,13 @@ class ImageServiceTest {
 
         @BeforeEach
         void prepare() {
-            existed_image = Image.builder().build();
+            Post post = postRepository.save(Post.builder().build());
+
+            existed_image = Image.builder()
+                    .postId(post.getId())
+                    .imageUrl(EXISTED_IMAGE_URL)
+                    .contentType(EXISTED_IMAGE_TYPE)
+                    .build();
             imageRepository.save(existed_image);
         }
 
